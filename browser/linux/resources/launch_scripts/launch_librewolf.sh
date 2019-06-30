22c47e0bb309f6e2d232177d6acfa47b4419276b
BeatLink <beatlink@protonmail.com>
2019-06-30 18:53:31 -0500

    add launch scripts

---
+++ browser/linux/resources/launch_scripts/launch_librewolf.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Adds option to install settings if argument is passed
+if [ "$1" = "--install-settings" ]; then
+    ./install_settings;
+fi
+
+# Sets env variables to disable dedicated profiles (which breaks in some cases)
+export MOZ_LEGACY_PROFILES=1;
+export SNAP_NAME="firefox";
+SCRIPT_FOLDER=$(realpath $(dirname $0));
+chmod +x $SCRIPT_FOLDER/librewolf;
+$SCRIPT_FOLDER/librewolf "$@"; 
+
+
+
