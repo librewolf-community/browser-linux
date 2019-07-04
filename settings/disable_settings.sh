fc3b107b7000ec24406d2e6d83bc8623b94c3246
BeatLink <beatlink@protonmail.com>
2019-07-04 03:18:05 -0500

    update settings script

--- settings/disable_settings.sh
+++ settings/disable_settings.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# This script will move the settings back to the storage folder, disabling it
+# Moves the settings from the main program folder to the settings folder, disabling it
 
-mv ../defaults/pref/local-settings.js ./defaults/pref/local-settings.js;
-mv ../distribution/policies.json ./distribution/policies.json;
-mv ../librewolf.cfg ./librewolf.cfg;
+SETTINGS_FOLDER=$(realpath $(dirname $0));
+PROGRAM_FOLDER=$SETTINGS_FOLDER/../;
+
+mv $PROGRAM_FOLDER/defaults/pref/local-settings.js $SETTINGS_FOLDER/defaults/pref/local-settings.js;
+mv $PROGRAM_FOLDER/distribution/policies.json      $SETTINGS_FOLDER/distribution/policies.json;
+mv $PROGRAM_FOLDER/librewolf.cfg                   $SETTINGS_FOLDER/librewolf.cfg;
 
--- settings/enable_settings.sh
+++ settings/enable_settings.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# Copies settings from the settings folder to the main program folder, enabling them
 
-cp ./defaults/pref/local-settings.js ../defaults/pref/local-settings.js;
-cp ./distribution/policies.json ../distribution/policies.json;
-cp ./librewolf.cfg ../librewolf.cfg;
+SETTINGS_FOLDER=$(realpath $(dirname $0));
+PROGRAM_FOLDER=$SETTINGS_FOLDER/../;
+
+mkdir -p $PROGRAM_FOLDER/defaults/pref/;
+mkdir -p $PROGRAM_FOLDER/distribution/;
+
+cp $SETTINGS_FOLDER/defaults/pref/local-settings.js $PROGRAM_FOLDER/defaults/pref/local-settings.js;
+cp $SETTINGS_FOLDER/distribution/policies.json      $PROGRAM_FOLDER/distribution/policies.json;
+cp $SETTINGS_FOLDER/librewolf.cfg                   $PROGRAM_FOLDER/librewolf.cfg;
