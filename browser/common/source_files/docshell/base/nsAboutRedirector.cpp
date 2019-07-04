fc395f7f2a43a6848781bf5e197f0512981a4ecc
BeatLink <beatlink@protonmail.com>
2019-07-04 02:08:01 -0500

    Add support for showing places at about:places

--- browser/common/source_files/docshell/base/nsAboutRedirector.cpp
+++ browser/common/source_files/docshell/base/nsAboutRedirector.cpp
@@ -67,7 +67,7 @@ static const RedirEntry kRedirMap[] = {
     {"about", "chrome://global/content/aboutAbout.xhtml", 0},
     {"addons", "chrome://mozapps/content/extensions/extensions.xul",
      nsIAboutModule::ALLOW_SCRIPT},
-    {"library", "chrome://browser/content/places/places.xul",
+    {"places", "chrome://browser/content/places/places.xul",
 	nsIAboutModule::ALLOW_SCRIPT},
     {"buildconfig", "chrome://global/content/buildconfig.html",
      nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT},
