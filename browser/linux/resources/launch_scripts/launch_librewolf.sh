a726cdad7e6dbbd5bb2e11910b2cae468b28f25e
BeatLink <beatlink@protonmail.com>
2019-07-04 01:43:06 -0500

    Comment out broken per profile install argument

--- browser/linux/resources/launch_scripts/launch_librewolf.sh
+++ browser/linux/resources/launch_scripts/launch_librewolf.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+# PROFILE SPECIFIC SETTINGS WILL NOT WORK, DO NOT UNCOMMENT
 # Adds option to install settings if argument is passed
-if [ "$1" = "--install-settings" ]; then
-    ./install_settings;
-fi
+# if [ "$1" = "--install-settings" ]; then
+#    ./install_settings;
+# fi
 
 # Sets env variables to disable dedicated profiles (which breaks in some cases)
 export MOZ_LEGACY_PROFILES=1;
