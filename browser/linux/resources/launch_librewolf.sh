bdaf8f6d226cb7ee5ec4fbd72ce75440a2bb1f93
BeatLink <beatlink@protonmail.com>
2019-07-14 13:10:10 -0500

    Move install settings to librewolf

--- browser/linux/resources/launch_librewolf.sh
+++ browser/linux/resources/launch_librewolf.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
 
-# PROFILE SPECIFIC SETTINGS WILL NOT WORK, DO NOT UNCOMMENT
-# Adds option to install settings if argument is passed
-# if [ "$1" = "--install-settings" ]; then
-#    ./install_settings;
-# fi
+INSTALL_FOLDER=$(realpath $(dirname $0));
+INSTALL_SETTINGS_FOLDER=$INSTALL_FOLDER/settings;
+PROFILE_SETTINGS_FOLDER=$HOME/.librewolf/settings;
 
-# Sets env variables to disable dedicated profiles (which breaks in some cases)
+# Adds option to install settings if argument is passed
+if [ "$1" = "--install-settings" ]; then
+    mkdir -p $PROFILE_SETTINGS_FOLDER;
+    cp $INSTALL_SETTINGS_FOLDER/defaults/pref/local-settings.js $PROFILE_SETTINGS_FOLDER/local-settings.js;
+    cp $INSTALL_SETTINGS_FOLDER/distribution/policies.json      $PROFILE_SETTINGS_FOLDER/policies.json;
+    cp $INSTALL_SETTINGS_FOLDER/librewolf.cfg                   $PROFILE_SETTINGS_FOLDER/librewolf.cfg;
+    cp $INSTALL_SETTINGS_FOLDER/toggle-settings.sh              $PROFILE_SETTINGS_FOLDER/toggle-settings.sh;
+fi
+
+# Sets up settings links
+ln -s $PROFILE_SETTINGS_FOLDER/local-settings.js $INSTALL_FOLDER/defaults/pref/local-settings.js;
+ln -s $PROFILE_SETTINGS_FOLDER/policies.json $INSTALL_FOLDER/distribution/policies.json;
+ln -s $PROFILE_SETTINGS_FOLDER/librewolf.cfg $INSTALL_FOLDER/librewolf.cfg;
+
+# Sets env variables to disable dedicated profiles (which breaks some packaging methods)
 export MOZ_LEGACY_PROFILES=1;
 export SNAP_NAME="firefox";
-SCRIPT_FOLDER=$(realpath $(dirname $0));
-chmod +x $SCRIPT_FOLDER/librewolf;
-$SCRIPT_FOLDER/librewolf "$@"; 
+
+# Launches librewolf
+chmod +x $INSTALL_FOLDER/librewolf;
+$INSTALL_FOLDER/librewolf "$@"; 
 
 
 
