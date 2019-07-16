87347601566d7b546310df0921b9ba020a92cd26
BeatLink <beatlink@protonmail.com>
2019-07-16 17:29:21 -0500

    fix flatpak build

--- browser/linux/build.sh
+++ browser/linux/build.sh
@@ -28,6 +28,7 @@ printf "\n\n-------------------------------------- PREBUILD --------------------
 
 # Installs some needed dependencies
 printf "\nInstalling script dependencies\n";
+apt install gnupg;
 echo 'deb http://ppa.launchpad.net/alexlarsson/flatpak/ubuntu bionic main' >>  /etc/apt/source.list;
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FA577F07;
 apt update;
