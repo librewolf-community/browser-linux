5458ba5fcc8fdc0f3746dc46444c420dc7f35027
psyozu <psyozu@posteo.net>
2019-07-17 04:58:47 +0530

    Make script comply with the standard bash scripting guidelines

--- settings/toggle-settings.sh
+++ settings/toggle-settings.sh
@@ -1,19 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Exit script with a non-zero exit code if:
+# - any command fails (-e | --errexit)
+# - any variable is unset (-u | --nounset)
+# - a part of a piped sequence fails (-o pipefail)
+set -euo pipefail
 
 SCRIPT_FOLDER=$(realpath $(dirname $0));
 
 # Enable settings ------------------------------------------------------------------------------------------
-if [ "$1" = "--enable" ]; then
-    mv $SCRIPT_FOLDER/"[DISABLED] local-settings.js"     $SCRIPT_FOLDER/local-settings.js;
-    mv $SCRIPT_FOLDER/"[DISABLED] policies.json"         $SCRIPT_FOLDER/policies.json;
-    mv $SCRIPT_FOLDER/"[DISABLED] librewolf.cfg"         $SCRIPT_FOLDER/librewolf.cfg;
-fi 
-
+if [[ "${1}" = "--enable" ]]; then
+    mv "${SCRIPT_FOLDER}/[DISABLED] local-settings.js"    "${SCRIPT_FOLDER}/local-settings.js";
+    mv "${SCRIPT_FOLDER}/[DISABLED] policies.json"        "${SCRIPT_FOLDER}/policies.json";
+    mv "${SCRIPT_FOLDER}/[DISABLED] librewolf.cfg"        "${SCRIPT_FOLDER}/librewolf.cfg";
 # Disable settings ------------------------------------------------------------------------------------------
-if [ "$1" = "--disable" ]; then
-    mv $SCRIPT_FOLDER/local-settings.js $SCRIPT_FOLDER/"[DISABLED] local-settings.js";
-    mv $SCRIPT_FOLDER/policies.json     $SCRIPT_FOLDER/"[DISABLED] policies.json";
-    mv $SCRIPT_FOLDER/librewolf.cfg     $SCRIPT_FOLDER/"[DISABLED] librewolf.cfg" ;
+elif [[ "${1}" = "--disable" ]]; then
+    mv "${SCRIPT_FOLDER}/local-settings.js" "${SCRIPT_FOLDER}/[DISABLED] local-settings.js";
+    mv "${SCRIPT_FOLDER}/policies.json"     "${SCRIPT_FOLDER}/[DISABLED] policies.json";
+    mv "${SCRIPT_FOLDER}/librewolf.cfg"     "${SCRIPT_FOLDER}/[DISABLED] librewolf.cfg";
 fi
-
-
