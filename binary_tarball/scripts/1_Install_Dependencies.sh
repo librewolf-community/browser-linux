440d7d323b32213d0356a0474736b6f32938b91d
ohfp <1813007-ohfp@users.noreply.gitlab.com>
2020-03-29 00:53:59 +0100

    install clang on 16.04

--- binary_tarball/scripts/1_Install_Dependencies.sh
+++ binary_tarball/scripts/1_Install_Dependencies.sh
@@ -3,9 +3,16 @@ printf "\n\n-------------------------------------- DEPENDENCY INSTALLATION -----
 
 # Setup Script Variables
 # _DEPENDENCIES="mercurial wget git flatpak flatpak-builder";
-_DEPENDENCIES="wget git xvfb xz-utils python3";
+_DEPENDENCIES="wget git xvfb build-essential xz-utils curl python3";
 
 # Installs Dependencies
 printf "\nInstalling dependencies: $_DEPENDENCIES\n";
 DEBIAN_FRONTEND=noninteractive apt-get -qq update;
 DEBIAN_FRONTEND=noninteractive apt-get -y -qq install $_DEPENDENCIES;
+
+# Installs (non-ancient) clang
+
+DEBIAN_FRONTEND=noninteractive apt install -y software-properties-common
+sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main"
+DEBIAN_FRONTEND=noninteractive wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add
+DEBIAN_FRONTEND=noninteractive apt-get -y install clang-9
