68d362cce739136964c1382a4b793cf53b7cf337
ohfp <1813007-ohfp@users.noreply.gitlab.com>
2020-03-29 14:42:06 +0200

    of course, nodejs is ancient as well

--- binary_tarball/scripts/1_Install_Dependencies.sh
+++ binary_tarball/scripts/1_Install_Dependencies.sh
@@ -6,15 +6,20 @@ set -e
 # Setup Script Variables
 # _DEPENDENCIES="mercurial wget git flatpak flatpak-builder";
 _DEPENDENCIES="wget git xvfb build-essential xz-utils curl python3 libjack-dev";
+export DEBIAN_FRONTEND=noninteractive
 
 # Installs Dependencies
 printf "\nInstalling dependencies: $_DEPENDENCIES\n";
-DEBIAN_FRONTEND=noninteractive apt-get -qq update;
-DEBIAN_FRONTEND=noninteractive apt-get -y -qq install $_DEPENDENCIES;
+apt-get -qq update;
+apt-get -y -qq install $_DEPENDENCIES;
 
 # Installs (non-ancient) clang
-DEBIAN_FRONTEND=noninteractive apt install -y software-properties-common apt-transport-https ca-certificates
+apt install -y software-properties-common apt-transport-https ca-certificates
 apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main"
-DEBIAN_FRONTEND=noninteractive wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add
-DEBIAN_FRONTEND=noninteractive apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get -y install clang-9 libclang-9-dev
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add
+apt-get update
+apt-get -y install clang-9 libclang-9-dev
+
+# Installs (non-ancient) clang
+curl -sL https://deb.nodesource.com/setup_13.x | bash -
+apt-get install -y nodejs
