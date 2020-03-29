31b77c08ce5151693bcfe16d2fb631e4a9cb3d53
ohfp <1813007-ohfp@users.noreply.gitlab.com>
2020-03-29 01:10:49 +0100

    bad c&p fixed (sudo)

--- binary_tarball/scripts/1_Install_Dependencies.sh
+++ binary_tarball/scripts/1_Install_Dependencies.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 printf "\n\n-------------------------------------- DEPENDENCY INSTALLATION ---------------------------------------------\n";
 
+set -e
+
 # Setup Script Variables
 # _DEPENDENCIES="mercurial wget git flatpak flatpak-builder";
 _DEPENDENCIES="wget git xvfb build-essential xz-utils curl python3";
@@ -13,7 +15,7 @@ DEBIAN_FRONTEND=noninteractive apt-get -y -qq install $_DEPENDENCIES;
 # Installs (non-ancient) clang
 
 DEBIAN_FRONTEND=noninteractive apt install -y software-properties-common
-sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main"
+apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main"
 DEBIAN_FRONTEND=noninteractive wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add
 DEBIAN_FRONTEND=noninteractive apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y install clang-9
