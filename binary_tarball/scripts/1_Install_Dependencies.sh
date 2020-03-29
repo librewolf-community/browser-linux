d0a7d8b4e2cc60de8b4c539f52bbd8ab423b994b
ohfp <1813007-ohfp@users.noreply.gitlab.com>
2020-03-29 01:14:10 +0100

    how hard can it be?

--- binary_tarball/scripts/1_Install_Dependencies.sh
+++ binary_tarball/scripts/1_Install_Dependencies.sh
@@ -14,7 +14,7 @@ DEBIAN_FRONTEND=noninteractive apt-get -y -qq install $_DEPENDENCIES;
 
 # Installs (non-ancient) clang
 
-DEBIAN_FRONTEND=noninteractive apt install -y software-properties-common
+DEBIAN_FRONTEND=noninteractive apt install -y software-properties-common apt-transport-https ca-certificates
 apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9 main"
 DEBIAN_FRONTEND=noninteractive wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add
 DEBIAN_FRONTEND=noninteractive apt-get update
