baa005aade99d74d36e48d07dc5684b9a05e4702
ohfp <1813007-ohfp@users.noreply.gitlab.com>
2020-03-30 11:40:19 +0200

    bzip2 dependency for appimage script

--- appimage/build_appimage.sh
+++ appimage/build_appimage.sh
@@ -16,7 +16,7 @@ _APPIMAGETOOL_FILE=$_SCRIPT_FOLDER/appimagetool;
 _APPIMAGE_CONTENT_FOLDER=$_SCRIPT_FOLDER/content
 
 # Installs needed dependencies
-apt-get update && apt-get -y install file wget;
+apt-get update && apt-get -y install file wget bzip2
 
 if [[ ! -z "${TARBALL_URL}" ]];then
   wget "${TARBALL_URL}"
