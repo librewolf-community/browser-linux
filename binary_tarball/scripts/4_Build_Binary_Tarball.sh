5d88f397b1d1e68c47f990e69ab51e33b8906aa0
ohfp <1813007-ohfp@users.noreply.gitlab.com>
2021-02-01 16:48:26 +0100

    source cargo env in build-script as well

--- binary_tarball/scripts/4_Build_Binary_Tarball.sh
+++ binary_tarball/scripts/4_Build_Binary_Tarball.sh
@@ -16,6 +16,8 @@ export DEB_BUILD_HARDENING_FORMAT=1
 export DEB_BUILD_HARDENING_PIE=1
 # export PATH=/usr/lib/nasm-mozilla/bin:$PATH
 
+source /root/.cargo/env
+
 # we do change / unset some of them later, but setting them as set by Arch
 # might make it easier to maintain changes in build scripts on both sides
 
