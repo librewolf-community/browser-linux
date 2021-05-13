b8677c649cfda41ad7828f156ae4a072f3ba874b
ohfp <1813007-ohfp@users.noreply.gitlab.com>
2021-05-13 14:49:52 +0200

    Generic patches have been moved to the common repo

--- scripts/3_Configure_Source_Code.sh
+++ scripts/3_Configure_Source_Code.sh
@@ -7,15 +7,20 @@ set -e
 srcdir=$1;
 CI_PROJECT_DIR=${CI_PROJECT_DIR:-$(realpath $(dirname $0)/../)}
 _COMMON_REPO='https://gitlab.com/librewolf-community/browser/common.git';
+_COMMON_COMMIT='5bce5285fa7046e6987ec3e5a8931ac17ca6c7c0'
+_COMMON_DIR="${CI_PROJECT_DIR}"/common
+_PATCHES_DIR="${_COMMON_DIR}"/patches
 _MOZBUILD=$srcdir/../mozbuild
 
 mkdir -p ${_MOZBUILD}
 
 # Copy Source Code Changes to Source Code
 printf "\nCopying branding and source code changes to firefox source code\n";
-git clone $_COMMON_REPO common;
-cp -r common/source_files/* $srcdir/;
-rm -rf common;
+git clone $_COMMON_REPO ${_COMMON_DIR}
+cd ${_COMMON_DIR}
+git checkout ${_COMMON_COMMIT}
+cd ..
+cp -r ${_COMMON_DIR}/source_files/* $srcdir/;
 
 cd $srcdir
 
@@ -85,9 +90,9 @@ END
   export RUSTFLAGS="-Cdebuginfo=0"
 
   export LDFLAGS+=" -Wl,--no-keep-memory -Wl"
-  patch -p1 -i ${CI_PROJECT_DIR}/arm.patch
-  wget https://raw.githubusercontent.com/archlinuxarm/PKGBUILDs/master/extra/firefox/build-arm-libopus.patch -O ${CI_PROJECT_DIR}/build-arm-libopus.patch
-  patch -p1 -i ${CI_PROJECT_DIR}/build-arm-libopus.patch
+  patch -Np1 -i ${_PATCHES_DIR}/arm.patch
+  wget https://raw.githubusercontent.com/archlinuxarm/PKGBUILDs/master/extra/firefox/build-arm-libopus.patch -O ${_PATCHES_DIR}/build-arm-libopus.patch
+  patch -Np1 -i ${_PATCHES_DIR}/build-arm-libopus.patch
 
 else
     cat >>${CI_PROJECT_DIR}/mozconfig <<END
@@ -110,51 +115,60 @@ END
 fi
 
 # hopefully the magic sauce that makes things build on 16.04 and later on work "everywhere":
-patch -p1 -i "${CI_PROJECT_DIR}/deb_patches/armhf-reduce-linker-memory-use.patch"
-patch -p1 -i "${CI_PROJECT_DIR}/deb_patches/fix-armhf-webrtc-build.patch"
-patch -p1 -i "${CI_PROJECT_DIR}/deb_patches/webrtc-fix-compiler-flags-for-armhf.patch"
-patch -p1 -i "${CI_PROJECT_DIR}/deb_patches/reduce-rust-debuginfo.patch"
-patch -p1 -i "${CI_PROJECT_DIR}/deb_patches/relax-cargo-dep.patch"
-patch -p1 -i "${CI_PROJECT_DIR}/deb_patches/use-system-icupkg.patch"
-patch -p1 -i "${CI_PROJECT_DIR}/deb_patches/sandbox-update-arm-syscall-numbers.patch"
+patch -Np1 -i "${CI_PROJECT_DIR}/deb_patches/armhf-reduce-linker-memory-use.patch"
+patch -Np1 -i "${CI_PROJECT_DIR}/deb_patches/fix-armhf-webrtc-build.patch"
+patch -Np1 -i "${CI_PROJECT_DIR}/deb_patches/webrtc-fix-compiler-flags-for-armhf.patch"
+patch -Np1 -i "${CI_PROJECT_DIR}/deb_patches/reduce-rust-debuginfo.patch"
+patch -Np1 -i "${CI_PROJECT_DIR}/deb_patches/relax-cargo-dep.patch"
+patch -Np1 -i "${CI_PROJECT_DIR}/deb_patches/use-system-icupkg.patch"
+patch -Np1 -i "${CI_PROJECT_DIR}/deb_patches/sandbox-update-arm-syscall-numbers.patch"
 
 # Remove some pre-installed addons that might be questionable
-patch -p1 -i ${CI_PROJECT_DIR}/remove_addons.patch
+patch -Np1 -i ${_PATCHES_DIR}/remove_addons.patch
 
 # Disable (some) megabar functionality
 # Adapted from https://github.com/WesleyBranton/userChrome.css-Customizations
-patch -p1 -i ${CI_PROJECT_DIR}/megabar.patch
+patch -Np1 -i ${_PATCHES_DIR}/megabar.patch
 
 # remove mozilla vpn ads
-patch -p1 -i ${CI_PROJECT_DIR}/mozilla-vpn-ad.patch
+patch -Np1 -i ${_PATCHES_DIR}/mozilla-vpn-ad.patch
 
 # Debian patch to enable global menubar
 if [[ ! -z "${GLOBAL_MENUBAR}" ]];then
-  patch -p1 -i ${CI_PROJECT_DIR}/unity-menubar.patch
+  patch -Np1 -i ${_PATCHES_DIR}/unity-menubar.patch
 fi
 
 # Disabling Pocket
 printf "\nDisabling Pocket\n";
-sed -i 's/"pocket"/# "pocket"/g' browser/components/moz.build
-
-patch -Np1 -i "${CI_PROJECT_DIR}/context-menu.patch"
-
+# sed -i 's/"pocket"/# "pocket"/g' browser/components/moz.build
 # this one only to remove an annoying error message:
 sed -i 's#SaveToPocket.init();#// SaveToPocket.init();#g' browser/components/BrowserGlue.jsm
+patch -Np1 -i "${_PATCHES_DIR}/sed-patches/disable-pocket.patch"
+
+# More patches
+patch -Np1 -i "${_PATCHES_DIR}/context-menu.patch"
+
+patch -Np1 -i "${_PATCHES_DIR}/browser-confvars.patch"
+patch -Np1 -i "${_PATCHES_DIR}/urlbarprovider-interventions.patch"
 
 # Remove Internal Plugin Certificates
-_cert_sed='s#if (aCert.organizationalUnit == "Mozilla [[:alpha:]]\+") {\n'
-_cert_sed+='[[:blank:]]\+return AddonManager\.SIGNEDSTATE_[[:upper:]]\+;\n'
-_cert_sed+='[[:blank:]]\+}#'
-_cert_sed+='// NOTE: removed#g'
-sed -z "$_cert_sed" -i toolkit/mozapps/extensions/internal/XPIInstall.jsm
+# _cert_sed='s#if (aCert.organizationalUnit == "Mozilla [[:alpha:]]\+") {\n'
+# _cert_sed+='[[:blank:]]\+return AddonManager\.SIGNEDSTATE_[[:upper:]]\+;\n'
+# _cert_sed+='[[:blank:]]\+}#'
+# _cert_sed+='// NOTE: removed#g'
+# sed -z "$_cert_sed" -i toolkit/mozapps/extensions/internal/XPIInstall.jsm
+patch -Np1 -i "${_PATCHES_DIR}/sed-patches/remove-internal-plugin-certs.patch"
 
 # allow SearchEngines option in non-ESR builds
-sed -i 's#"enterprise_only": true,#"enterprise_only": false,#g' browser/components/enterprisepolicies/schemas/policies-schema.json
+# sed -i 's#"enterprise_only": true,#"enterprise_only": false,#g' browser/components/enterprisepolicies/schemas/policies-schema.json
+patch -Np1 -i "${_PATCHES_DIR}/sed-patches/allow-searchengines-non-esr.patch"
 
 # stop some undesired requests (https://gitlab.com/librewolf-community/browser/common/-/issues/10)
-_settings_services_sed='s#firefox.settings.services.mozilla.com#f.s.s.m.c.qjz9zk#g'
-sed "$_settings_services_sed" -i browser/components/newtab/data/content/activity-stream.bundle.js
-sed "$_settings_services_sed" -i modules/libpref/init/all.js
-sed "$_settings_services_sed" -i services/settings/Utils.jsm
-sed "$_settings_services_sed" -i toolkit/components/search/SearchUtils.jsm
+# _settings_services_sed='s#firefox.settings.services.mozilla.com#f.s.s.m.c.qjz9zk#g'
+# sed "$_settings_services_sed" -i browser/components/newtab/data/content/activity-stream.bundle.js
+# sed "$_settings_services_sed" -i modules/libpref/init/all.js
+# sed "$_settings_services_sed" -i services/settings/Utils.jsm
+# sed "$_settings_services_sed" -i toolkit/components/search/SearchUtils.jsm
+patch -Np1 -i "${_PATCHES_DIR}/sed-patches/stop-undesired-requests.patch"
+
+rm -rf common
