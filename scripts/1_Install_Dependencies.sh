f34e1245964a709fd0b16e56ee7fadeb768067ce
ohfp <1813007-ohfp@users.noreply.gitlab.com>
2021-08-30 13:44:56 +0200

    Revert "tmp output to debug ci/env/gitlab issue"
    This reverts commit bb4d2b8c3bd2484b9549e75cf4cf4a986516a76b.
    
    => was wrong / had a typo anyway

--- scripts/1_Install_Dependencies.sh
+++ scripts/1_Install_Dependencies.sh
@@ -3,12 +3,6 @@ printf "\n\n-------------------------------------- DEPENDENCY INSTALLATION -----
 
 set -e
 
-# debuggery
-
-printf '----- dbg: env'
-env
-printf '----- /dbg: env'
-
 # Setup Script Variables
 _DEPENDENCIES="wget git xvfb \
  xz-utils \
